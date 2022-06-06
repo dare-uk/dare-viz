0b70ed759cc14c728e3fbd759dd4edaaf46ecbad
Matt Rigby <matt.rigby@bristol.ac.uk>
2022-06-06 06:07:22 +0100

    Added Jupyter Book template

---
+++ decc-report/_build/jupyter_execute/emissions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # DECC network emissions
+
+# In[1]:
+
+
+from IPython.display import display, HTML, IFrame
+
+display(HTML("InTEM_CO2e_2021.html", "<style>.container { width:60% !important; }</style>"))
+#IFrame('InTEM_CO2e_2021.html', width=800, height=630)
+
+
+# ```{tableofcontents}
+# ```
+
+# In[ ]:
+
+
+
+
